757b169aac1928a8202edd0264d9894dc5f5bdbb
HauXun <hautrieu65@gmail.com>
2022-04-11 22:14:09 +0700

    finished Login UI

---
+++ BookStore/BookStore/Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace BookStore
+{
+	public partial class Login : Form
+	{
+		public Login()
+		{
+			InitializeComponent();
+		}
+	}
+}
