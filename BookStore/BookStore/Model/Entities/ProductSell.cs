709270bd5dc427b1fbcf82b9e050d1c7cb209604
HauXun <hautrieu65@gmail.com>
2022-04-26 22:06:18 +0700

    product sell entity

---
+++ BookStore/BookStore/Model/Entities/ProductSell.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.Model.Entities
+{
+	public class ProductSell
+	{
+		public int MaSP { get; set; }
+		public string TenSP { get; set; }
+		public int SoLuong { get; set; }
+		public float TongTien { get; set; }
+
+		public ProductSell()
+		{
+
+		}
+
+		public ProductSell(DataRow row)
+		{
+			MaSP = (int)row["MaSP"];
+			TenSP = row["TenSP"].ToString();
+			SoLuong = (int)row["SoLuong"];
+			if (row.Table.Columns.Count == 4)
+				TongTien = float.Parse(row["TongTien"].ToString());
+		}
+	}
+}
