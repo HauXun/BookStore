2d84e78b1b6c17de514d4b6713d867be5e6013cf
HauXun <hautrieu65@gmail.com>
2022-05-05 22:55:48 +0700

    account bll class

---
+++ BookStore/BookStore/Model/BusinessLogicLayer/AccountBLL.cs
@@ -0,0 +1,42 @@
+﻿using BookStore.Model.DataAccessLayer;
+using BookStore.Model.Entities;
+using System.Data;
+
+namespace BookStore.Model.BusinessLogicLayer
+{
+	public class AccountBLL
+	{
+		private static AccountBLL instance;
+
+		public static AccountBLL Instance
+		{
+			get
+			{
+				if (instance == null)
+					instance = new AccountBLL();
+				return instance;
+			}
+			private set => instance = value;
+		}
+
+		public Taikhoan GetAccount(string idAccount, string password)
+		{
+			return AccountDAL.Instance.GetAccount(idAccount, password);
+		}
+
+		public int CreateAccount(Taikhoan taikhoan)
+		{
+			return AccountDAL.Instance.CreateAccount(taikhoan);
+		}
+
+		public int CancleAccount(string name, string sdt)
+		{
+			return AccountDAL.Instance.CancleAccount(name, sdt);
+		}
+
+		public DataTable GetAllAccount()
+		{
+			return AccountDAL.Instance.GetAllAccount();
+		}
+	}
+}
