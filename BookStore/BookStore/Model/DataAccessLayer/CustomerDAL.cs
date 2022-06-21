5f1882af5500bb47ae841b7332d26948ff6ee124
HauXun <hautrieu65@gmail.com>
2022-06-21 10:29:06 +0700

    customer dal

--- BookStore/BookStore/Model/DataAccessLayer/CustomerDAL.cs
+++ BookStore/BookStore/Model/DataAccessLayer/CustomerDAL.cs
@@ -1,6 +1,7 @@
 ﻿using BookStore.Extensions;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,10 +23,32 @@ namespace BookStore.Model.DataAccessLayer
 			private set => instance = value;
 		}
 
-		public object InsertDoiTac(string tenDT, string diaChi, string sdt)
+		public DataTable GetAllCustomer(bool isCustomer = true)
 		{
-			string query = "EXEC dbo.USP_InsertDoiTac @TenDT , @DiaChi , @SDT";
-			return DataProvider.Instance.ExcuteScalar(query, new object[] { tenDT, diaChi, sdt });
+			string query = "SELECT * FROM dbo.DoiTac WHERE MaDT LIKE 'K%'";
+			if (isCustomer)
+			{
+				query = "SELECT * FROM dbo.DoiTac WHERE MaDT LIKE 'C%'";
+			}
+			return DataProvider.Instance.ExcuteQuery(query);
+		}
+
+		public DataTable InsertDoiTac(string tenDT, string diaChi, string sdt, bool isCustomer = true)
+		{
+			string query = "EXEC dbo.USP_InsertDoiTac @TenDT , @DiaChi , @SDT , @isCustomer";
+			return DataProvider.Instance.ExcuteQuery(query, new object[] { tenDT, diaChi, sdt, isCustomer });
+		}
+
+		public DataTable SearchDoiTac(string keyword, bool isCustomer = true)
+		{
+			string query = $"USP_SearchDoiTac @keyword , @isCustomer";
+			return DataProvider.Instance.ExcuteQuery(query, new object[] { keyword, isCustomer });
+		}
+
+		public DataTable GetDoiTacByInfo(string tenDT, string diaChi, string sdt)
+		{
+			string query = "EXEC dbo.USP_GetDoiTacByInfo @TenDT , @DiaChi , @SDT";
+			return DataProvider.Instance.ExcuteQuery(query, new object[] { tenDT, diaChi, sdt });
 		}
 	}
 }
