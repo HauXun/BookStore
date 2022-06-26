1a2d123bf08f62e5e5b41238054a71c23e125dfd
HauXun <hautrieu65@gmail.com>
2022-06-26 10:03:06 +0700

    sửa lại lớp hóa đơn

--- BookStore/BookStore/Model/Entities/HoaDon.cs
+++ BookStore/BookStore/Model/Entities/HoaDon.cs
@@ -9,42 +9,51 @@
 
 namespace BookStore.Model.Entities
 {
-    using System;
-    using System.Collections.Generic;
+	using System;
+	using System.Collections.Generic;
 	using System.Data;
 
 	public partial class HoaDon
-    {
-        public int? MaHoaDon { get; set; }
-        public System.DateTime NgayGiaoDich { get; set; }
-        public string MaDT { get; set; }
-        public string LoaiHoaDon { get; set; }
-        public decimal GiamGia { get; set; }
-        public double TongTien { get; set; }
+	{
+		public int? MaHoaDon { get; set; }
+		public string NguoiBan { get; set; }
+		public System.DateTime NgayGiaoDich { get; set; }
+		public string MaDT { get; set; }
+		public string LoaiHoaDon { get; set; }
+		public decimal GiamGia { get; set; }
+		public double TongTien { get; set; }
+		public bool Status { get; set; }
 
 		public HoaDon()
 		{
 
 		}
 
-		public HoaDon(DataRow row)
+		public HoaDon(DataRow row, DataTable data = null)
 		{
-            MaHoaDon = (int)row["MaHoaDon"];
-            NgayGiaoDich = Convert.ToDateTime(row["NgayGiaoDich"]);
-            MaDT = row["MaDT"].ToString();
-            LoaiHoaDon = row["LoaiHoaDon"].ToString();
-            GiamGia = decimal.Parse(row["GiamGia"].ToString());
-            TongTien = (double)row["TongTien"];
-        }
-
-		public HoaDon(DateTime ngayGiaoDich, string maDT, string loaiHoaDon, decimal giamGia, double tongTien, int? maHoaDon = null)
-		{
-            MaHoaDon = maHoaDon;
-            NgayGiaoDich = ngayGiaoDich;
-            MaDT = maDT;
-            LoaiHoaDon = loaiHoaDon;
-            GiamGia = giamGia;
-            TongTien = tongTien;    
+			MaHoaDon = (int)row["MaHoaDon"];
+			if (data != null && data.Columns.Contains("NguoiBan"))
+				NguoiBan = row["NguoiBan"].ToString();
+			NgayGiaoDich = Convert.ToDateTime(row["NgayGiaoDich"]);
+			MaDT = row["MaDT"].ToString();
+			LoaiHoaDon = row["LoaiHoaDon"].ToString();
+			GiamGia = decimal.Parse(row["GiamGia"].ToString());
+			TongTien = (double)row["TongTien"];
+			if (data != null && data.Columns.Contains("Status"))
+				Status = (bool)row["Status"];
 		}
-    }
+
+		public HoaDon(string nguoiBan, DateTime ngayGiaoDich, string maDT, string loaiHoaDon, decimal giamGia,
+			double tongTien, bool status = false, int? maHoaDon = null)
+		{
+			MaHoaDon = maHoaDon;
+			NguoiBan = nguoiBan;
+			NgayGiaoDich = ngayGiaoDich;
+			MaDT = maDT;
+			LoaiHoaDon = loaiHoaDon;
+			GiamGia = giamGia;
+			TongTien = tongTien;
+			Status = status;
+		}
+	}
 }
